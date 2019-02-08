d7a0223c480d6127ffbdd573e2e210d34be6d2e7
YamamotaSempai <aidos.jantleu@gmail.com>
2019-02-08 20:53:57 +0600

    megre

--- app/src/main/java/com/example/noclip/appnavshop/HomeFragment.java
+++ app/src/main/java/com/example/noclip/appnavshop/HomeFragment.java
@@ -19,10 +19,13 @@ public class HomeFragment extends Fragment {
     }
 
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        System.out.println("asdsadasdasasd");
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
